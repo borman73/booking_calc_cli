bcf29fb147db4084085e9d9bb9c8db3269650d9f
borman73 <zk-borman@yandex.ru>
2023-01-25 21:44:13 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Booking Calculator CLI
+
+Training app calculating the cost of hotel accommodation for a given period
+
+Ruby version -'3.1.3'
+
+
---
+++ booking_calc.rb
@@ -0,0 +1,68 @@
+require 'date'
+
+WEEKDAY_COST = 1750
+WEEKEND_COST = 2500
+
+class Calculator
+
+  attr_reader :checkin_date, :days 
+ 
+  def initialize(checkin_date, days)
+    @checkin_date = checkin_date
+    @days = days
+  end
+
+  def calculate(checkin_date, days)
+    checkout_date = (checkin_date + days)
+
+    period = Array(checkin_date..checkout_date)
+    period.shift
+
+    cost = period.reduce(0) do |total, day| 
+      if day.saturday? || day.sunday? 
+        total + WEEKEND_COST
+      else
+        total + WEEKDAY_COST 
+      end
+    end
+  end
+
+end
+
+puts "\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+puts "\nЗдравствуйте! Давайте рассчитаем стоимость пребывания вашего пребывания в отеле."
+puts "\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+checkin_date = ""
+days = 0
+
+loop do
+  puts "\nВведите дату заезда в формате день.месяц.год (например 08.03.2023):"
+  input_date = gets.strip
+
+  if input_date.empty? 
+    puts "\n Вы ничего не ввели" 
+  elsif Date.strptime(input_date, "%d.%m.%Y") < Date.today
+    puts "\nДата не должна быть из прошлого"
+  else
+    checkin_date = Date.strptime(input_date, "%d.%m.%Y")
+    break  
+  end
+end
+
+loop do 
+  puts "\nВведите количество дней пребывания в отеле:"
+  input_days = gets.strip.to_i
+  
+  if input_days < 1
+    puts "Минимальный срок пребывания - 1 день" 
+  else
+    days = input_days
+    break
+  end
+end
+
+calc = Calculator.new(checkin_date, days)
+puts "\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+puts "Стоимость проживания составит: #{calc.calculate(calc.checkin_date, calc.days)} руб."
+
